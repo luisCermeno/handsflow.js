9525a52b168fc143bf1889103a5a396a01fa2e11
luisCermeno <luis.cermeno.f@gmail.com>
2023-03-22 18:47:14 -0700

    TODO: implement async checkplay line48

--- Engine.js
+++ Engine.js
@@ -1,43 +1,53 @@
 export default class Engine{
   // CONSTRUCTOR
   constructor(n_players){
-    this.n_players = n_players
-    this.turn = 0
-    this.gameover = false
+    var plays_obj = {}
+    for(let i = 0; i < n_players; i++){
+      plays_obj[i] = false
+    }
+
+    this.gamestate = {
+      n_players: n_players,
+      turn: 0,
+      gameover: false,
+      plays: plays_obj
+    }
+    
     this.onupdate = (new_state) => {console.log(`New state: ${new_state}`)}
     this.onplay = (player,play) => {console.log(`Player ${player} played ${play}`)}
   }
 
+
   // HELPER PRIVATE METHODS
   sleep = ms => new Promise(resolve => setTimeout(() => {resolve()}, ms));
-  
-  async run() {
-    console.log('Game started')
-    // Print first turn
-    // Start game loop
-    while (this.gameover == false) {
-      this.turn = (this.turn + 1) % this.n_players
-      this.onupdate(this.turn)
-      await this.sleep(2000)
-    }
+  update(new_state) {
+    this.gamestate = new_state
+    this.onupdate(new_state)
   }
 
   // PUBLIC METHODS
   play(player, play) {
-    if (this.gameover) {
+    if (this.gamestate.gameover == true) {
       return
     }
     // Log the play
     this.onplay(player,play)
-
+    this.update({...this.gamestate, plays: {...this.gamestate.plays, [player]: true}})
     // Check for gameover
-    if (this.turn != player) {
-      this.gameover = true
+    if (this.gamestate.turn != player) {
+      this.update({...this.gamestate, gameover: true})
     }
   }
 
-  start() {
-    this.run()
+  async start() {
+    console.log('Game started')
+    // Print first turn
+    // Start game loop
+    while (this.gamestate.gameover == false) {
+      this.update({...this.gamestate, turn: (this.gamestate.turn + 1) % this.gamestate.n_players})
+      await this.sleep(2000) // TODO: await for checkplay!!s
+
+    }
   }
 
 }
--- script.js
+++ script.js
@@ -6,18 +6,24 @@ const log = document.querySelector('.log')
 
 // UI HANDLERS
 function updateUI (state) {
-  gamestate.innerHTML = state.toString()
+  gamestate.innerHTML = state.turn.toString()
+  let new_log = 'Plays: '
+
+  for(const [k, v] of Object.entries(state.plays)) {
+    new_log = new_log + `[${k}:${v}]`
+  }
+  log.innerHTML = new_log
 }
-function updatelog(player,play) {
-  const new_log = document.createElement('div')
-  new_log.innerHTML = `Player ${player} played ${play}`
-  log.append(new_log)
-};
+// function updatelog(player,play) {
+//   const new_log = document.createElement('div')
+//   new_log.innerHTML = `Player ${player} played ${play}`
+//   log.append(new_log)
+// };
 
 // START A GAME
 const game = new Engine(4)
 game.onupdate = updateUI
-game.onplay = updatelog
+// game.onplay = updatelog
 game.start()
 
 // Listen for local player play
