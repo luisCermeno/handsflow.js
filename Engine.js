74af8bf0315cf5de9bc850dbd25f584160dc83a5
luisCermeno <luis.cermeno.f@gmail.com>
2023-03-24 17:22:16 -0700

    add players list, reference player by id, turn is index in players/

--- Engine.js
+++ Engine.js
@@ -2,13 +2,16 @@ export default class Engine{
   // CONSTRUCTOR
   constructor(n_players){
     var plays_obj = {}
-    for(let i = 0; i < n_players; i++){
-      plays_obj[i] = false
+    var players_l = []
+    for(let id = 0; id < n_players; id++){
+      plays_obj[id] = false
+      players_l.push(id)
     }
 
     this.plays = plays_obj
     this.gamestate = {
       n_players: n_players,
+      players: players_l,
       turn: 0,
       gameover: false,
     }
@@ -27,40 +30,41 @@ export default class Engine{
 
   update(new_state) {
     this.onupdate(new_state)
-    this.gamestate = new_state
+    this.gamestate = new_state    
   }
-  update_plays(player,play) {
-    this.onplay(player,play)
-    this.plays = {...this.plays, [player]: play}
+  update_plays(id,play) {
+    this.onplay(id,play)
+    this.plays = {...this.plays, [id]: play}
   }
   
 
   // PUBLIC METHODS
-  play(player, play) {
+  play(id, play) {
     if (this.gamestate.gameover == true) {
       return
     }
     // Check for gameover
-    if (play == null || this.gamestate.turn != player) {
-      this.update({...this.gamestate, gameover: true})
+    if (play == null || this.gamestate.turn != id) {
+      this.update({...this.gamestate, gameover: true}) // todo: remove player
     }
     // Log the play
-    this.update_plays(player,play)
+    this.update_plays(id,play)
 
   }
 
-  waitplay = player => new Promise(async resolve => {
+  waitplay = turn => new Promise(async resolve => {
     let ms = 0
     let play = 0
+    let id = this.gamestate.players[turn]
     while (play == 0 & ms < 500) {
       // AI always plays after half the time allowed
-      if (ms > 250 & player != 0) {
-        this.play(player, this.generate_play())
+      if (ms > 250 & id != 0) {
+        this.play(turn, this.generate_play())
       }
       // CHECK for play recorded in gamestate. Reset to false if found
-      if (this.plays[player] != 0) {
-        play = this.plays[player]
-        this.plays = {...this.plays, [player] : 0}
+      if (this.plays[id] != 0) {
+        play = this.plays[id]
+        this.plays = {...this.plays, [id] : 0}
       }
       // Wait one ms and continue
       await this.sleep(1)
@@ -76,12 +80,13 @@ export default class Engine{
     // Start game loop
     let curr_play = 1
     while (this.gamestate.gameover == false) {
-      this.update({...this.gamestate, turn: this.mod(this.gamestate.turn + curr_play, this.gamestate.n_players) })
+      let new_turn = this.mod(this.gamestate.turn + curr_play, this.gamestate.n_players)
+      this.update({...this.gamestate, turn: new_turn})
       // Wait for play
       curr_play = await this.waitplay(this.gamestate.turn)
-      // After waiting for play, gameover is user didnt play (TODO: remove one hand!)
+      // After waiting for play
       if (curr_play == 0) {
-        this.update({...this.gamestate, gameover: true})
+        this.update({...this.gamestate, gameover: true}) // todo: remove player
       }
     }
   }
