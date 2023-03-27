0a41ae923e689027fda21142fe1d726ec3c899ba
luisCermeno <luis.cermeno.f@gmail.com>
2023-03-27 15:45:35 -0700

    enhance console.logs

--- bot.js
+++ bot.js
@@ -10,12 +10,12 @@ export default class Bot{
   async listen_state(new_state){
     // If its bot's turn, disptach play.
     if (new_state.request_id == null & new_state.players[new_state.index] == this.id) {
-      console.log(`BOT ${this.id}: My turn!`)
       // Get a random delay time to simulate human doubt behaviour.
       var delay = this.tools.randint(this.game.waittime / 4 , this.game.waittime)
       // Wait half the waiting time before plays
       await this.tools.sleep(delay)
       // Dispatch play
+      console.log(`BOT ${this.id}: My turn!`)
       this.game.play(this.id, this.tools.generate_play(this.game.moves))
     }
   }
