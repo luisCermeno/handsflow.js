3afdbc342905765bf16b2eeb370fdd35c30d256f
luisCermeno <luis.cermeno.f@gmail.com>
2023-03-27 17:14:17 -0700

    initial implementation of record class for plays

--- Record.js
+++ Record.js
@@ -1,8 +1,23 @@
 export default class Record{
   constructor(ids) {
+    // Construct mp {id: last play}
     var mp = {}
     for(const id in ids){
       mp[id] = null
+    }
+  }
+  // Returns true if a play of record is found
+  has_played = (id) => this.plays[id] != null
+  // Returns the play of a player and clears it
+  use_play(id) {
+    play = this.plays[id]
+    this.plays[id] = null
+    return play
+  }
+  reset() {
+    // Reset all plays to 0.
+    for (const id in this.plays) {
+      this.plays[id] = null
     }
   }
 }
